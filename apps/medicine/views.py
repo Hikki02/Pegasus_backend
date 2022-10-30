4089d5496747bd436edc5835381f58ac37f86331
Hikki02 <a.aidaraliev25@gmail.com>
2022-10-30 18:26:30 +0500

    fix vaciantion

--- apps/medicine/views.py
+++ apps/medicine/views.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import User
 from django.shortcuts import get_object_or_404
 from rest_framework import generics
 
-from .models import Medication
+from .models import Medication, Vaccine
 from .serializers import MedicationSerializer, VaccineSerializer
 from rest_framework import viewsets
 from rest_framework.response import Response
@@ -21,10 +21,10 @@ class MedicationRetrieveUpdateDestroy(generics.RetrieveUpdateDestroyAPIView):
 
 class VaccineCreateList(generics.ListCreateAPIView):
     serializer_class = VaccineSerializer
-    queryset = Medication.objects.filter()
+    queryset = Vaccine.objects.filter()
 
 
 class VaccineRetrieveUpdateDestroy(generics.RetrieveUpdateDestroyAPIView):
     serializer_class = VaccineSerializer
-    queryset = Medication.objects.filter()
+    queryset = Vaccine.objects.filter()
 
